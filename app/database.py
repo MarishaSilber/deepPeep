0d2a43a15bc977deeb86f5afe37d698780ca2bad
KirKol30 <colesnicenko.c@yandex.ru>
2025-04-19 17:04:25 +0300

    first without venv

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:yourpassword@localhost:5432/my_new_db"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:299792458@localhost/icrbd"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
